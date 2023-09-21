79963c400a894f57561bd4c9cc7cb718e3754d34
frcomparan <frcomparan@gmail.com>
2023-09-21 16:20:31 -0600

    Refactor .ino code

--- U2_Hardware_y_software_para_IoT/RequestPost/RequestPost.ino
+++ U2_Hardware_y_software_para_IoT/RequestPost/RequestPost.ino
@@ -16,29 +16,35 @@ String serverName = "http://5bdc-187-190-35-202.ngrok-free.app";
 unsigned long lastTime = 0;
 unsigned long timerDelay = 3000;
 
-// Define los pines para los botones
-const int buttonPinAsc = 2; // Cambia al número de pin que estás utilizando
-const int buttonPinDesc = 4; // Cambia al número de pin que estás utilizando
-
+const int buttonPinAsc = 2;
+const int buttonPinDesc = 4;
 const int ledPin = 5;
 
-bool ascRequested = false; // Bandera para controlar la solicitud "asc"
-bool descRequested = false; // Bandera para controlar la solicitud "desc"
+bool ascRequested = false;
+bool descRequested = false;
 
-void setup() {
-  Serial.begin(115200);
-  dht.begin();
+// Function to set up the WiFi connection
+void setupWiFi() {
+  Serial.println("Connecting to WiFi...");
   WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
+
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+
   Serial.println("");
   Serial.print("Connected to WiFi network with IP Address: ");
   Serial.println(WiFi.localIP());
+}
 
-  // Configura los pines de los botones como entradas
+// Function to set up pins and initializations
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+
+  setupWiFi();
+
   pinMode(buttonPinAsc, INPUT_PULLUP);
   pinMode(buttonPinDesc, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
@@ -46,156 +52,140 @@ void setup() {
   Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
-void loop() {
-  // Temperatura
-  float temperature = dht.readTemperature();
-  float humidity = dht.readHumidity();
+// Function to handle sending sensor data to the server
+void sendDataToServer(String path, String key, float value) {
+  HTTPClient http;
+  http.begin((serverName + path).c_str());
 
-  if (isnan(temperature)) {
-    Serial.println("Error al leer el sensor DHT!");
-    return;
+  DynamicJsonDocument jsonDoc(64);
+  jsonDoc[key] = value;
+
+  String jsonString;
+  serializeJson(jsonDoc, jsonString);
+
+  http.addHeader("Content-Type", "application/json");
+
+  int httpResponseCode = http.POST(jsonString);
+
+  if (httpResponseCode > 0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String payload = http.getString();
+    Serial.println(payload);
+  } else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
   }
 
-  // Verifica si el botón "asc" se ha presionado
+  http.end();
+}
+
+// Function to handle LED status
+void handleLEDStatus() {
+  String serverPathLed = serverName + "/led";
+  HTTPClient http;
+  http.begin(serverPathLed.c_str());
+
+  int httpResponseCodeGET = http.GET();
+
+  if (httpResponseCodeGET > 0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCodeGET);
+    String payload = http.getString();
+    Serial.println(payload);
+
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, payload);
+
+    int led = doc["led"];
+
+    if (led == 0) {
+      digitalWrite(ledPin, LOW);
+    }
+    if (led == 1) {
+      digitalWrite(ledPin, HIGH);
+    }
+  } else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCodeGET);
+  }
+
+  http.end();
+}
+
+// Function to handle the button actions
+void handleButtonActions() {
   if (digitalRead(buttonPinAsc) == HIGH && !ascRequested) {
     ascRequested = true;
   }
 
-  // Verifica si el botón "desc" se ha presionado
   if (digitalRead(buttonPinDesc) == HIGH && !descRequested) {
     descRequested = true;
   }
+}
+
+// Function to send the action to the server
+void sendActionToServer(String action, int quantity) {
+  String serverPath = serverName + "/led";
+  HTTPClient http;
+  http.begin(serverPath.c_str());
+
+  DynamicJsonDocument jsonDoc(64);
+  jsonDoc["action"] = action;
+  jsonDoc["quantity"] = quantity;
+
+  String jsonStringCounter;
+  serializeJson(jsonDoc, jsonStringCounter);
+
+  http.addHeader("Content-Type", "application/json");
+
+  int httpResponseCode = http.POST(jsonStringCounter);
+
+  if (httpResponseCode > 0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    String payload = http.getString();
+    Serial.println(payload);
+  } else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  http.end();
+}
+
+void loop() {
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  if (isnan(temperature)) {
+    Serial.println("Error reading DHT sensor!");
+    return;
+  }
+
+  handleButtonActions();
 
   if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
+    sendDataToServer("/temperature", "temperature", temperature);
+    sendDataToServer("/humidity", "humidity", humidity);
 
-    // Envio del POST de la temperatura
-    String serverPath = serverName + "/temperature";
-
-    http.begin(serverPath.c_str());
-
-    DynamicJsonDocument jsonDocTemp(64);
-
-    jsonDocTemp["temperature"] = temperature; // Cambia la cantidad según tu necesidad
-
-    String jsonString;
-    serializeJson(jsonDocTemp, jsonString);
-
-    http.addHeader("Content-Type", "application/json");
-
-    int httpResponseCode = http.POST(jsonString);
-
-    if (httpResponseCode > 0) {
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-      String payload = http.getString();
-      Serial.println(payload);
-    } else {
-      Serial.print("Error code: ");
-      Serial.println(httpResponseCode);
-    }
-
-    http.end();
-
-    serverPath = serverName + "/humidity";
-
-    http.begin(serverPath.c_str());
-
-    DynamicJsonDocument jsonDocHum(64);
-
-    jsonDocHum["humidity"] = humidity; // Cambia la cantidad según tu necesidad
-
-    String jsonStringHum;
-    serializeJson(jsonDocHum, jsonStringHum);
-
-    http.addHeader("Content-Type", "application/json");
-
-    httpResponseCode = http.POST(jsonStringHum);
-
-    if (httpResponseCode > 0) {
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-      String payload = http.getString();
-      Serial.println(payload);
-    } else {
-      Serial.print("Error code: ");
-      Serial.println(httpResponseCode);
-    }
-
-    http.end();
-
-    // Get del status del led
-    String serverPathLed = serverName + "/led";
-
-    http.begin(serverPathLed.c_str());
-
-    // Send HTTP GET request
-    int httpResponseCodeGET = http.GET();
-    
-    if (httpResponseCodeGET>0) {
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCodeGET);
-      String payload = http.getString();
-      Serial.println(payload);
-
-      DynamicJsonDocument doc(1024);
-      deserializeJson(doc, payload);
-
-      int led = doc["led"];
-
-      if (led == 0) {
-        digitalWrite(ledPin, LOW);
-      }
-      if (led == 1) {
-        digitalWrite(ledPin, HIGH);
-      }
-    } else {
-      Serial.print("Error code: ");
-      Serial.println(httpResponseCodeGET);
-    }
-
-    http.end();
+    handleLEDStatus();
 
     if (ascRequested || descRequested) {
       if ((millis() - lastTime) > timerDelay) {
-          serverPath = serverName + "/led";
-
-          http.begin(serverPath.c_str());
-
-          DynamicJsonDocument jsonDoc(64);
-
-          if (ascRequested) {
-            jsonDoc["action"] = "asc";
-            ascRequested = false; // Restablece la bandera después de la solicitud
-          } else if (descRequested) {
-            jsonDoc["action"] = "desc";
-            descRequested = false; // Restablece la bandera después de la solicitud
-          }
-          jsonDoc["quantity"] = 1; // Cambia la cantidad según tu necesidad
-
-          String jsonStringCounter;
-          serializeJson(jsonDoc, jsonStringCounter);
-
-          http.addHeader("Content-Type", "application/json");
-
-          httpResponseCode = http.POST(jsonStringCounter);
-
-          if (httpResponseCode > 0) {
-            Serial.print("HTTP Response code: ");
-            Serial.println(httpResponseCode);
-            String payload = http.getString();
-            Serial.println(payload);
-          } else {
-            Serial.print("Error code: ");
-            Serial.println(httpResponseCode);
-          }
-
-          http.end();
+        if (ascRequested) {
+          sendActionToServer("asc", 1);
+          ascRequested = false;
+        } else if (descRequested) {
+          sendActionToServer("desc", 1);
+          descRequested = false;
+        }
         lastTime = millis();
       }
     }
   } else {
     Serial.println("WiFi Disconnected");
   }
+
   delay(500);
 }
